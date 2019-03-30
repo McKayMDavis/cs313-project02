ec2ceb7fc324b54cbb666a36737eea3bf919c529
McKay Davis <mckaymdavis@gmail.com>
2019-03-29 19:48:56 -0700

    stuff

--- controllers/displayControllers.js
+++ controllers/displayControllers.js
@@ -2,6 +2,7 @@ const model = require("../models/dataManipulation.js");
 
 // These functions are for displaying pages
 
+// We could either put a condition in here to redirect to the viz page if logged in or we could make viz html for a two page app
 function displayLogin(req, res) {
 	res.sendFile(path.join(__dirname, './public/index.html'));
 }
@@ -27,26 +28,60 @@ function displayAddUser(req, res) {
 // These functions are for handling data requests for display purposes
 
 function displayData(req, res) {
-	model.pullData("SELECT * FROM expense", (rows) => {
+	var table = req.body.dtype;
+	var year = req.body.year;
+	var query = "SELECT expense_id, description, vendor, amount, year, date_entered, last_update FROM " + table + " WHERE year = '" + year + "'";
+
+	var sql = "SELECT expense_id, description, vendor, amount, year, date_entered, last_update FROM $1 WHERE year = $2";
+	var params = [table, year];
+
+	model.pullData(query, (rows) => {
 		console.log(rows.keys());
 		//rows = parseTable(rows);
-		res.render("vizWindow.ejs", { data: rows[0] });
+		//res.render("vizWindow.ejs", { data: rows[0] });
+		res.json(rows);
 	});
 }
 
 function displayAddRowsTable(req, res) {
-	model.pullData("SELECT attrelid::regclass AS tbl, attname AS col, atttypid::regtype AS datatype\
+	var query = "SELECT attrelid::regclass AS tbl, attname AS col, atttypid::regtype AS datatype\
 					FROM pg_attribute\
 					WHERE attrelid = 'expense'::regclass\
 					AND attnum > 0\
 					AND NOT attisdropped\
-					ORDER BY attnum", (rows) => {
+					ORDER BY attnum";
+
+	model.pullData(query, (rows) => {
 		console.log(rows);
 		res.render("addWindow.ejs", { data: rows[0] });
 	});
 }
 
 
+
+// Handle login
+function login(req, res) {
+	var user = req.body.username;
+	var pass = req.body.password;
+	var query = "SELECT user_id, username, password, user_type FROM users WHERE username = '" + user + "';";
+
+	model.pullData(query, (rows) => {
+		if (rows[0]) {
+			console.log("Password found: Setting session var");
+			req.session.uid = rows[0].user_id;
+			console.log(req.session.uid);
+			res.render('viz.ejs');
+		} else {
+			//do nothing
+		}
+	});
+}
+
+function logout(req, res) {
+	req.session.destroy();
+	console.log("Session destroyed");
+	res.redirect('back');
+}
 
 
 
@@ -57,5 +92,7 @@ module.exports = {
 	displayAddRows: displayAddRows,
 	displayAddUser: displayAddUser,
 	displayData: displayData,
-	displayAddRowsTable: displayAddRowsTable
+	displayAddRowsTable: displayAddRowsTable,
+	login: login,
+	logout: logout
 };
--- server.js
+++ server.js
@@ -4,19 +4,44 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const session = require('express-session');
+app.use(session({
+	secret: 'uid-session',
+	resave: false,
+	saveUninitialized: true
+}));
+
 app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+/**********************
+Login Check Middleware
+**********************/
+function checkLogin(req, res, next) {
+	if (req.session.uid) {
+		next();
+	} else {
+		res.status(401).json({success: false, message: 'User not authorized.'});
+	}
+}
+
 /***********
 Web Services
 ***********/
-app.get('/', controller.displayLogin);
+app.post('/login', controller.login);
+app.get('/logout', controller.logout);
+
+
+app.get('/', checkLogin, controller.displayLogin);
 //app.get('/header', controller.displayHeader);
-app.get('/viz', controller.displayViz);
-app.get('/addRows', controller.displayAddRows);
-app.get('/addUser', controller.displayAddUser);
-app.post('/displayData', controller.displayData);
+app.get('/viz', checkLogin, controller.displayViz);
+app.get('/addRows', checkLogin, controller.displayAddRows);
+app.get('/addUser', checkLogin, controller.displayAddUser);
+app.post('/displayData', checkLogin, controller.displayData);
 //app.post('/displayAddRowsTable', controller.displayAddRowsTable);
 
 app.listen(port, () => console.log("Server running"));
